bab391e4b5dc93ab6907bf77395dc46b5b76a9d6
Lukas Benke <benkel02@dixonscarphone.com>
2020-06-18 09:56:38 +0200

    Walking skeleton done

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Value {
+  id: number,
+  name: string
+}
+
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  async componentDidMount() {
+    try {
+      const values : Value[] = (await axios.get('http://localhost:5000/api/values')).data
+
+      this.setState({
+        values: values
+      })
+    } catch (e) {
+      alert('Error')
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            {this.state.values.map((value: Value) =>
+              (
+                <li key={value.id}>{value.name}</li>
+              )
+            )}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
